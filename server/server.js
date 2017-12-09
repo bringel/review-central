544433f42da1da3a54e6fcb3d79535c30ee7196e
Brad Ringel <brad.ringel@gmail.com>
2017-12-09 15:55:34 -0500

    Remove passport auth because it wasnt working. replace with regular oauth package

--- server/server.js
+++ server/server.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const passport = require('passport');
-const GithubStrategy = require('passport-github2').Strategy;
+const OAuth2 = require('oauth').OAuth2;
 
 const app = express();
 const port = process.env.PORT || 3000;
+const callbackURL = 'http://127.0.0.1:3000/login/github/callback';
 
 app.use(express.static(path.join(__dirname, 'dist')));
-passport.use(new GithubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://127.0.0.1:3000/api/login/github/callback'
-  },
-  function (accessToken, refreshToken, profile, cb) {
-    console.log(`Access token: ${accessToken} refreshToken: ${refreshToken}, profile: ${profile}`);
-    return cb(profile);
-  }));
 
-app.get('/api/login/github', passport.authenticate('github'));
+app.get('/login/github', function(req, res) {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${callbackURL}`);
+});
 
-app.get('/api/login/github/callback',
- passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-    console.log('successful auth');
-    res.redirect('/');
+app.get('/login/github/callback', function(request, response) {
+
+  const code = request.query.code;
+  const oauthClient = new OAuth2(process.env.GITHUB_CLIENT_ID, process.env.GITHUB_CLIENT_SECRET, 'https://github.com', '/login/oauth/authorize', '/login/oauth/access_token');
+
+  oauthClient.getOAuthAccessToken(code, {}, function (e, access_token, refresh_token, results){
+    console.log('bearer: ',access_token);
+    response.send(access_token);
   });
-
+}
+);
 
 app.listen(port, function() {
   console.log(`app listening on port ${port}`);
